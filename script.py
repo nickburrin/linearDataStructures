11d5218eab40290a30daa2312783839f955022e8
NickBurrin <nick.burrin@faithlife.com>
2019-07-14 21:53:31 -0500

    Rewrite remove_node method in a way that makes more sense to me

--- script.py
+++ script.py
@@ -34,6 +34,15 @@ class LinkedList:
             curr = curr.get_next_node()
         return curr;
 
+    def get_node_with_value(self, value):
+        curr = self.head_node;
+        while curr.get_value() != value:
+            if curr.get_next_node() == None:
+                print('** Could not find node with value: {} **'.format(value));
+                return None;
+            curr = curr.get_next_node();
+        return curr;
+
     def stringify_list(self):
         current = self.head_node;
         output = "";
@@ -43,22 +52,27 @@ class LinkedList:
         output += str(current.get_value()) + "\n";
         return output;
 
-    # Rewrite using single while-loop
     def remove_node(self, value_to_remove):
-        current_node = self.get_head_node();
-        if current_node.get_value() == value_to_remove:
-            self.head_node = current_node.get_next_node()
+        print('Removing node with value: {}'.format(value_to_remove));
+        
+        current = self.head_node;
+        next = current.get_next_node()
+        if current.get_value == value_to_remove:
+            self.head_node = next;
         else:
-            while current_node:
-                next_node = current_node.get_next_node()
-                if next_node.get_value() == value_to_remove:
-                    current_node.set_next_node(next_node.get_next_node());
-                    current_node = None;
-                else:
-                    current_node = next_node;
+            while next.get_value() != value_to_remove:
+                current = next;
+                next = next.get_next_node();
+                if next == None:
+                    print('No node to remove with value: {}'.format(value_to_remove));
+                    return;
+            current.set_next_node(next.get_next_node());
 
 ll = LinkedList(5);
 ll.insert_beginning(2);
 ll.insert_end(20);
 ll.insert_end(25);
+print(ll.stringify_list());
+
+ll.remove_node(25);
 print(ll.stringify_list());

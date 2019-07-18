65599da92a8c6a1022bcb9ebd5dd845bb478fbe7
NickBurrin <nick.burrin@faithlife.com>
2019-07-17 22:16:00 -0700

    Implements Queues

--- script.py
+++ script.py
@@ -1,27 +1,38 @@
-from Stack import Stack;
+from Queue import Queue;
 
-# Defining an empty pizza stack with limit of 6
-pizza_stack = Stack(6)
+print("Creating a deli line with up to 10 orders...\n------------")
+deli_line = Queue(10)
+print("Adding orders to our deli line...\n------------")
+deli_line.enqueue("egg and cheese on a roll")
+deli_line.enqueue("bacon, egg, and cheese on a roll")
+deli_line.enqueue("toasted sesame bagel with butter and jelly")
+deli_line.enqueue("toasted roll with butter")
+deli_line.enqueue("bacon, egg, and cheese on a plain bagel")
+deli_line.enqueue("two fried eggs with home fries and ketchup")
+deli_line.enqueue("egg and cheese on a roll with jalapeos")
+deli_line.enqueue("plain bagel with plain cream cheese")
+deli_line.enqueue("blueberry muffin toasted with butter")
+deli_line.enqueue("bacon, egg, and cheese on a roll")
 
-# Adding pizzas as they are ready until we have 
-pizza_stack.push("pizza #1")
-pizza_stack.push("pizza #2")
-pizza_stack.push("pizza #3")
-pizza_stack.push("pizza #4")
-pizza_stack.push("pizza #5")
-pizza_stack.push("pizza #6")
+# ------------------------ #
+# Uncomment the line below:
+# deli_line.enqueue("western omelet with home fries")
+# ------------------------ #
 
-# Uncomment the push() statement below:
-#pizza_stack.push("pizza #7")
+print("------------\nOur first order will be " + deli_line.peek())
+print("------------\nNow serving...\n------------")
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
 
-# Delivering pizzas from the top of the stack down
-print("The first pizza to deliver is " + pizza_stack.peek())
-pizza_stack.pop()
-pizza_stack.pop()
-pizza_stack.pop()
-pizza_stack.pop()
-pizza_stack.pop()
-pizza_stack.pop()
-
-# Uncomment the pop() statement below:
-#pizza_stack.pop()
+# ------------------------ #
+# Uncomment the line below:
+# deli_line.dequeue()
+# ------------------------ #
